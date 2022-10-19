9bfdd7b646f471fba26c3452f6681e2ed45bb15a
Robinson Urdaneta <rurdaneta@mutualentreasoc.com.ar>
2022-10-19 12:27:52 -0300

    03-JS-III/homework/homework.js

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,48 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var contador = 0;
+    for(var i = 1; i < 11; i++ ){contador = contador + i;}
+    return contador;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado = [];
+  var indice = 0;
+  while( indice < array.length){ 
+    if (array[indice] % 2 === 0){ resultado.push(array[indice]);
+    }
+    indice++;
+    }
+    return resultado;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function (num){
+    return Math.pow(num, 2);
+  })
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var result = array.reduce(function (contador , num) {return contador + num; 
+  }, 0);
+  return result
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var CantidadDigitos = num + "a";
+  return CantidadDigitos.length - 1;
 }
   
   
